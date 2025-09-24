f99a4fffb9e5773524bc6ac29b3f3a6f1a0ddc2e
raquelizek <raquelreclizek@gmail.com>
2025-09-24 20:35:20 -0300

    [ADD] oza_seed: add initial __init__.py and __manifest__.py files for OZA Seed module

---
+++ custom-addons/oza_seed/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  "name": "OZA Seed",
+  "version": "1.0",
+  "depends": ["base", "contacts", "project"], 
+  "data": [
+    "data/project.project.csv",
+    "data/project.task.csv",
+    "data/res.partner.csv",
+  ],
+  "installable": True,
+}
