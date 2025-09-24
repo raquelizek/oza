124fefac86de5fae9ad693c85b2abdc22ab7a1f9
raquelizek <raquelreclizek@gmail.com>
2025-09-24 19:39:16 -0300

    [ADD] oza_branding: add initial manifest and init files for OZA Branding module

---
+++ custom-addons/oza_branding/__manifest__.py
@@ -0,0 +1,11 @@
+{
+  "name": "OZA Branding",
+  "version": "1.0",
+  "depends": ["web"],
+  "assets": {
+    "web.assets_backend": [
+      "oza_branding/static/src/scss/oza_branding.scss",
+    ],
+  },
+  "installable": True,
+}
