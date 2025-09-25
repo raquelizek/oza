da6e0ff8e487816db40f4019b24c2dd258433758
raquelizek <raquelreclizek@gmail.com>
2025-09-24 21:14:55 -0300

    [IMP] manifest: reorder data files in __manifest__.py for clarity

--- custom-addons/oza_seed/__manifest__.py
+++ custom-addons/oza_seed/__manifest__.py
@@ -1,11 +1,11 @@
 {
   "name": "OZA Seed",
   "version": "1.0",
-  "depends": ["base", "contacts", "project"], 
+  "depends": ["base", "contacts", "project"],
   "data": [
+    "data/res.partner.csv",
     "data/project.project.csv",
     "data/project.task.csv",
-    "data/res.partner.csv",
   ],
   "installable": True,
 }
