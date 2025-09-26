56ae9304490dfdf90d2a6a05f8cbf577a265208d
raquelizek <raquelreclizek@gmail.com>
2025-09-25 22:00:23 -0300

    [IMP] manifest: update version and add data files; add favicon and logo images; enhance SCSS for branding

--- custom-addons/oza_branding/__manifest__.py
+++ custom-addons/oza_branding/__manifest__.py
@@ -1,11 +1,17 @@
 {
-  "name": "OZA Branding",
-  "version": "1.0",
-  "depends": ["web"],
-  "assets": {
-    "web.assets_backend": [
-      "oza_branding/static/src/scss/oza_branding.scss",
+    "name": "OZA Branding",
+    "version": "1.2.7",
+    "depends": ["web"],
+    "data": [
+        "views/web_layout.xml",  
     ],
-  },
-  "installable": True,
+    "assets": {
+        "web.assets_backend": [
+            "oza_branding/static/src/scss/oza_branding.scss",
+        ],
+        "web.assets_frontend": [
+            "oza_branding/static/src/scss/oza_branding.scss",
+        ],
+    },
+    "installable": True,
 }
