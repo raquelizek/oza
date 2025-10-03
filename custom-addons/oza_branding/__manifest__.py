58548762ea375103fe8f451d3140aa615b785927
raquelizek <raquelreclizek@gmail.com>
2025-10-02 22:25:53 -0300

    [IMP] branding: update version, add app icons, and enhance web layout with new favicon and metadata

--- custom-addons/oza_branding/__manifest__.py
+++ custom-addons/oza_branding/__manifest__.py
@@ -1,17 +1,24 @@
 {
     "name": "OZA Branding",
-    "version": "1.2.7",
+    "version": "19.0.1.0.0",
+    "summary": "Troca textos e identidade visual de Odoo para OZA",
     "depends": ["web"],
     "data": [
-        "views/web_layout.xml",  
+        "data/app_icons.xml",
+        "views/web_layout.xml",
+        "data/ir_config_parameter.xml",
     ],
     "assets": {
+        "web.assets_frontend": [
+            "oza_branding/static/src/scss/oza_branding.scss",
+        ],
         "web.assets_backend": [
             "oza_branding/static/src/scss/oza_branding.scss",
         ],
-        "web.assets_frontend": [
+        "web.report_assets_common": [
             "oza_branding/static/src/scss/oza_branding.scss",
         ],
     },
     "installable": True,
+    "application": False,
 }
